ace608ec4ac075b50ba6cf207b3c316252ecdfc9
Yurii <50027889+WeiZedInc@users.noreply.github.com>
2023-03-10 21:35:56 +0200

    LoginView added

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace CrossPlatformChat;
+﻿using CrossPlatformChat.MVVM.Views;
+
+namespace CrossPlatformChat;
 
 public partial class App : Application
 {
@@ -6,6 +8,6 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-		MainPage = new AppShell();
+		MainPage = new LoginView();
 	}
 }
---
+++ MVVM/Views/LoginView.xaml.cs
@@ -0,0 +1,9 @@
+namespace CrossPlatformChat.MVVM.Views;
+
+public partial class LoginView : ContentPage
+{
+	public LoginView()
+	{
+		InitializeComponent();
+	}
+}
