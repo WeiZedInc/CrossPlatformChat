94368b8cb2b899af31e98e986dd043a46ae185d9
Yurii <50027889+WeiZedInc@users.noreply.github.com>
2023-03-11 20:21:09 +0200

    db migration added (using MySQL localdb)

--- Chat.API/Entities/ChatAppContext.cs
+++ Chat.API/Entities/ChatAppContext.cs
@@ -1,9 +1,16 @@
-﻿namespace Chat.API.Entities
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Chat.API.Entities
 {
     public class ChatAppContext : DbContext
     {
         public ChatAppContext(DbContextOptions<ChatAppContext> options) : base(options) { }
-
         public virtual DbSet<Users> Users { get; set; } = null!;
+
+        public static readonly string connectionString = "Server=localhost; User ID=root; Password=root; Database=chatapp";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
     }
 }
--- Chat.API/Program.cs
+++ Chat.API/Program.cs
@@ -1,40 +1,40 @@
+
 using Chat.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
 
-namespace Chat.API
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ChatAppContext>(option =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    option.UseMySql(ChatAppContext.connectionString, ServerVersion.AutoDetect(ChatAppContext.connectionString));
+});
 
-            // Add services to the container.
+builder.Services.AddTransient<MySqlConnection>(_ =>
+new MySqlConnection(ChatAppContext.connectionString));
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+var app = builder.Build();
 
-            builder.Services.AddDbContext<ChatAppContext>
-                (options => options.UseSqlServer(builder.Configuration["ConnectionString"]));
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
