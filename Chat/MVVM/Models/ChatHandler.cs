bf08cbdb5df9b7261cfa51df4827d06e7b4ef093
Yurii <50027889+WeiZedInc@users.noreply.github.com>
2023-03-13 21:23:23 +0200

    prepeared models for SQLite db

--- Chat/MVVM/Models/ChatHandler.cs
+++ Chat/MVVM/Models/ChatHandler.cs
@@ -1,73 +1,25 @@
-﻿using CrossPlatformChat.MVVM.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace CrossPlatformChat.MVVM.Models
 {
-    internal class ChatHandler : INotifyPropertyChanged
+    partial class ChatHandler : ObservableObject
     {
-        int _ID;
-        string _ChatName;
-        string _ChatLogo;
-        int _LastMessageID;
-        string _LastChatMessage;
-        bool _IsOneByOne;
-        int _UsersQuantity;
-        DateTime _ChatCreatedDate;
-        public int ID 
-        {
-            get => _ID;
-            set { _ID = value; OnPropertyChanged(); }
-        }
-        public string ChatName
-        {
-            get => _ChatName;
-            set { _ChatName = value; OnPropertyChanged(); }
-        } 
-        public string ChatLogo
-        {
-            get => _ChatLogo;
-            set { _ChatLogo = value; OnPropertyChanged(); }
-        } 
-        public int LastMessageID
-        {
-            get => _LastMessageID;
-            set { _LastMessageID = value; OnPropertyChanged(); }
-        }
-        public string LastChatMessage
-        {
-            get => _LastChatMessage;
-            set { _LastChatMessage = value; OnPropertyChanged(); }
-        }
-        public bool IsOneByOne
-        {
-            get => _IsOneByOne;
-            set { _IsOneByOne = value; OnPropertyChanged(); }
-        }
-        public int UsersQuantity
-        {
-            get => _UsersQuantity;
-            set { _UsersQuantity = value; OnPropertyChanged(); }
-        }
-        public DateTime ChatCreatedDate
-        {
-            get => _ChatCreatedDate;
-            set { _ChatCreatedDate = value; OnPropertyChanged(); }
-        } 
+        [ObservableProperty]
+        int id;
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        [ObservableProperty]
+        int missedMsgsCount;
 
-        public ChatHandler()
-        {
-            _ChatName = "New chat";
-            _ChatLogo = "dotnet_bot.svg";
-            _LastChatMessage = "UserName: test";
-            _ChatCreatedDate = DateTime.Now;
-        }
+        [ObservableProperty]
+        string name;
+
+        [ObservableProperty]
+        string usersID; //convert array of users id's to JSON
+
+        [ObservableProperty]
+        string messagesID; //convert array of messages id's to JSON
+
+        [ObservableProperty]
+        DateTime createdDate;
     }
 }
---
+++ Chat/MVVM/Models/MessageHandler.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossPlatformChat.MVVM.Models
+{
+    public enum MessageStatus
+    {
+        Read = 0,
+        Received,
+        SentAndNotRead
+    }
+    partial class MessageHandler : ObservableObject
+    {
+        [ObservableProperty]
+        int id;
+
+        [ObservableProperty]
+        string userID; // who sent id
+
+        [ObservableProperty]
+        string text;
+
+        [ObservableProperty]
+        int chatID; // belongs to wich chat
+
+        [ObservableProperty]
+        DateTime sentDate;
+
+        [ObservableProperty]
+        MessageStatus status;
+    }
+}
